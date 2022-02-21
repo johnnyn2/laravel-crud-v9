b15d4f1529eb716fe9ec9876050b6d6658426b3c
wai kin ho <wkhoae@connect.ust.hk>
2022-02-21 16:43:08 +0800

    feat: web routes, logic handling in controller, passing parameters to view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function() {
+    return view('home');
+});
+
+Route::get('/products', [ProductsController::class, 'index']);
+
+Route::get('/string', function() {
+    return 'this is a string';
+});
+
+Route::get('/array', function() {
+    return ['this', 'is', 'array'];
+});
+
+Route::get('/json', function() {
+    return response()->json([
+        'name' => 'Laravel-crud-v9',
+        'framework' => 'laravel'
+    ]);
+});
+
+Route::get('/redirect', function() {
+    return redirect('/redirectedPage');
+});
+
+Route::get('/redirectedPage', function() {
+    return 'redirected';
 });
